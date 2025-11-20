d62bc960728d54f139e53b4c64685267b8f4ac23
mukesh57-git <withersolo57@gmail.com>
2025-11-20 15:11:45 +0530

    day4 code pushed

---
+++ day4/3_variables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int age = 30;
+    printf("your age is: %d" , age);
+    float pi = 3.14;
+    printf("\nthe value of pi is: %d" , pi);
+    double wieght = 1.24;
+    printf("\nthe wieght is: %lf" , wieght);
+    char name[50] = "xyz";
+    printf("\nname is: %s" , name );
+    bool iswalking = false;
+    printf("\n is walking: %d" , iswalking);
+}
